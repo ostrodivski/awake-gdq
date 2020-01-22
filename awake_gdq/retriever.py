71b17d522f6de14d83ff0b36a65013b4396c6721
xeb <guillaume.redero@wanadoo.fr>
2020-01-22 23:25:29 +0100

    just foget the ! in the shebangs

--- awake_gdq/retriever.py
+++ awake_gdq/retriever.py
@@ -1,4 +1,4 @@
-#\usr\bin\python3
+#!\usr\bin\python3
 
 import urllib3
 from bs4 import BeautifulSoup
--- debug/main.py
+++ debug/main.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 import tkinter as tk
 from debug.debug_app import DebugApplication
